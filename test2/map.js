4ec5a7ccc9ba05730172d837290df33d0b4178b6
id10bat <my_nutthapat-26_@hotmail.com>
2017-07-28 23:10:12 +0700

    map2

---
+++ test2/map.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+  //mapArea  
+  $('#mapArea').html("<div style='background-color:#bcfef1;width:100%;height:100%;'><div style='background-color:#ffffff;width:10%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:12%;'></div><div id='zone-A' style='background-color:#c0c0c0;width:40%;height:80%;float:right;'></div></div> <div style='background-color:#ffffff;width:100%;height:12%;'></div><div style='background-color:#e29d36;width:100%;height:80%;bottom:0%;'><div style='background-color:#f381df;width:100%;height:90%;'><div style='background-color:#e6f381;width:100%;height:80%;'> <div style='background-color:#e28b2c;width:80%;height:100%;float:right;'><div style='background-color:#57e0e8;width:25%;height:100%;float:left;'><div id='zone-B' style='background-color:#c0c0c0;width:20%;height:100%;float:left;'></div><div style='background-color:#ffffff;width:100%;height:85%;'></div><div id='zone-C' style='background-color:#c0c0c0;width:100%;height:15%;bottom:0%;'></div></div><div style='background-color:#abf0f3;width:15%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:50%;'></div></div><div style='background-color:#073234;width:60%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:85%;'></div><div id='zone-D' style='background-color:#c0c0c0;width:100%;height:15%;bottom:0%;'></div></div></div><div style='background-color:#ffffff;width:10%;height:100%;float:left;'></div></div><div style='background-color:#ffffff;width:100%;height:20%;bottom:0%;'></div></div><div id='zone-E' style='background-color:#c0c0c0;width:100%;height:10%;bottom:0%;'></div><div style='background-color:#ffffff;width:100%;height:10%;bottom:0%;'></div></div>");
+
+  // zone-A
+  $('#zone-A').html("<button style='width:100%;height:3%;' id='part1'></button><button style='width:100%;height:3%;' id='part2'></button>");
+  $('#part1').click(function () {
+  });
+  $('#part2').click(function() {});
+
+  // zone-B
+
+
+   // zone-C
+
+
+    // zone-D
+
+
+     // zone-E
+
+
+
+});
+
+

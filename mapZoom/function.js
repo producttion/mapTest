53d81087e04462a46ee1ae158769a283e5a48e3c
id10bat <my_nutthapat-26_@hotmail.com>
2017-07-29 23:08:50 +0700

    config database

---
+++ mapZoom/function.js
@@ -0,0 +1,51 @@
+
+
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB6bQk1TDpCH3ylmFL3HHbqMjqMapiLZpU",
+  authDomain: "test-c5a03.firebaseapp.com",
+  databaseURL: "https://test-c5a03.firebaseio.com",
+  projectId: "test-c5a03",
+  storageBucket: "test-c5a03.appspot.com",
+  messagingSenderId: "919585579120"
+};
+firebase.initializeApp(config);
+
+var dbRef = firebase.database().ref()
+
+function clickOn() {
+  $('.cbtn').click(function () {
+    var text = $(this).attr("value")
+
+    dbRef.child(`users/data/${text}`)
+      .set({
+        test: text
+      })
+    alert(text);
+
+
+  })
+}
+
+var getdbRef = firebase.database().ref(`users/data/10/test`);
+
+var getValueRef = getdbRef.on('value', function (snapshot) {
+  var snap = snapshot.val();
+  var num0 = 0;
+  console.log(snap)
+  if (snap == null){
+    ('.cbtn').css({ 'background-color': '#' });
+  }else{
+    $('.cbtn').css({ 'background-color': '#f44336' });
+  }
+
+  if (getdbRef == getdbRef) {
+    $('.cbtn').css({ 'background-color': '#f44336' });
+  } else {
+    $('.cbtn').css({ 'background-color': '#' });
+  }
+})
+
+
--- mapZoom/map.js
+++ mapZoom/map.js
@@ -1,12 +1,9 @@
 
 
-$(document).ready(function () {
+ $(document).ready(function () {
 
   //mapArea  
   $('#mapArea').html("<div style='background-color:#bcfef1;width:100%;height:100%;'><div style='background-color:#ffffff;width:10%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:12%;'></div><div id='zone-A' class='zoomItem' style='background-color:#c0c0c0;width:40%;height:80%;float:right;'></div></div> <div style='background-color:#ffffff;width:100%;height:12%;'></div><div  class='zoomItem' style='background-color:#e29d36;width:100%;height:80%;bottom:0%;'><div style='background-color:#f381df;width:100%;height:90%;'><div style='background-color:#e6f381;width:100%;height:80%;'> <div style='background-color:#e28b2c;width:80%;height:100%;float:right;'><div style='background-color:#57e0e8;width:25%;height:100%;float:left;'><div id='zone-B' class='zoomItem' style='background-color:#c0c0c0;width:20%;height:100%;float:left;'></div><div class='zoomItem' style='background-color:#ffffff;width:100%;height:85%;'></div><div id='zone-C' class='zoomItem' style='background-color:#c0c0c0;width:100%;height:15%;'></div></div><div  class='zoomItem' style='background-color:#abf0f3;width:15%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:50%;'></div></div><div style='background-color:#073234;width:60%;height:100%;float:left;'><div style='background-color:#ffffff;width:100%;height:85%;'></div><div id='zone-D' class='zoomItem' style='background-color:#c0c0c0;width:100%;height:15%;bottom:0%;'></div></div></div><div class='zoomItem' style='background-color:#ffffff;width:10%;height:100%;float:left;'></div></div><div class='zoomItem' style='background-color:#ffffff;width:100%;height:20%;bottom:0%;'></div></div><div id='zone-E' class='zoomItem' style='background-color:#c0c0c0;width:100%;height:10%;bottom:0%;'></div><div class='zoomItem' style='background-color:#ffffff;width:100%;height:10%;bottom:0%;'></div></div>");
-
-  // zone-A
-  // $('#zone-A').html("<input type='button' value='boundary' class='zoomItem' style='font-size:50%; width:100%;height:3%;'/><input type='button' value='boundary' class='zoomItem' style='font-size:50%; width:100%;height:3%;' id='part2'/>");
 
   var zoneA = $('#zone-A');
   var zoneB = $('#zone-B');
@@ -27,104 +24,96 @@ $(document).ready(function () {
   var buttonZoneE = 39;
 
   $(document).ready(function () {
+
+
     //  Zone A
     var endZoneA = setInterval(function () {
+      
       if (buttonZoneA == createButtonZA) {
         clearInterval(endZoneA);
       }
-      zoneA.append("<input type='button' value=" + 'boundary' + buttonZoneA + " class='zoomItem'  style='font-size:50%; width:100%;height:9%;'/>");
+      zoneA.append("<button class='cbtn' onClick='clickOn()' value=" + buttonZoneA + " class='zoomItem'  style='font-size:50%; width:100%;height:9%;'> "+'boundary' + buttonZoneA +"</button");
       var newListA = $('#zone-A');
       newListA.fadeIn();
       var colorEnd = setInterval(function () {
         newListA.css('color', 'black');
         clearInterval(colorEnd);
-      }, 35);
+      }, 0);
       buttonZoneA = buttonZoneA + 1;
-    }, 30);
+      
+    }, 0);
 
 
-  // ZoneB
-  var endZoneB = setInterval(function () {
+    // ZoneB
+    var endZoneB = setInterval(function () {
       if (buttonZoneB == createButtonZB) {
         clearInterval(endZoneB);
       }
-      zoneB.append("<input type='button' value=" + 'boundary' + buttonZoneB + " class='zoomItem'  style='font-size:50%; width:100%;height:12%;'/>");
+      zoneB.append("<button class='cbtn' onClick='clickOn()' type='button' value="+ buttonZoneB + " class='zoomItem'  style='font-size:50%; width:100%;height:12%;'>"+'boundary' + buttonZoneB +"</button>");
       var newListB = $('#zone-B');
       newListB.fadeIn();
       var colorEnd = setInterval(function () {
         newListB.css('color', 'black');
         clearInterval(colorEnd);
-      }, 35);
+      }, 0);
       buttonZoneB = buttonZoneB + 1;
-    }, 30);
+    }, 0);
 
     // ZoneC
-  var endZoneC = setInterval(function () {
+    var endZoneC = setInterval(function () {
       if (buttonZoneC == createButtonZC) {
         clearInterval(endZoneC);
       }
-      zoneC.append("<input type='button' value=" + 'boundary' + buttonZoneC + " class='zoomItem'  style='font-size:50%; width:16%;height:100%;'/>");
+      zoneC.append("<button class='cbtn' onClick='clickOn()' type='button' value="+ buttonZoneC + " class='zoomItem'  style='font-size:50%; width:16%;height:100%;'>"+'boundary' + buttonZoneC +"</button>");
       var newListC = $('#zone-C');
       newListC.fadeIn();
       var colorEnd = setInterval(function () {
         newListC.css('color', 'black');
         clearInterval(colorEnd);
-      }, 35);
+      }, 0);
       buttonZoneC = buttonZoneC + 1;
-    }, 30);
+    }, 0);
 
-      // ZoneD
-  var endZoneD = setInterval(function () {
+    // ZoneD
+    var endZoneD = setInterval(function () {
       if (buttonZoneD == createButtonZD) {
         clearInterval(endZoneD);
       }
-      zoneD.append("<input type='button' value=" + 'boundary' + buttonZoneD + " class='zoomItem'  style='font-size:50%; width:7%;height:100%;'/>");
+      zoneD.append("<button class='cbtn' onClick='clickOn()' type='button' value=" + buttonZoneD + " class='zoomItem'  style='font-size:50%; width:7%;height:100%;'>"+'boundary' + buttonZoneD +"</button>");
       var newListD = $('#zone-D');
       newListD.fadeIn();
       var colorEnd = setInterval(function () {
         newListD.css('color', 'black');
         clearInterval(colorEnd);
-      }, 35);
+      }, 0);
       buttonZoneD = buttonZoneD + 1;
-    }, 30);
+    }, 0);
 
-         // ZoneE
-  var endZoneE = setInterval(function () {
+    // ZoneE
+    var endZoneE = setInterval(function () {
       if (buttonZoneE == createButtonZE) {
         clearInterval(endZoneE);
       }
-      zoneE.append("<input type='button' value=" + 'boundary' + buttonZoneE + " class='zoomItem'  style='font-size:50%; width:3.3%;height:100%;'/>");
+      zoneE.append("<button class='cbtn' onClick='clickOn()' type='button' value=" + buttonZoneE + " class='zoomItem'  style='font-size:50%; width:3.3%;height:100%;'>"+'boundary' + buttonZoneE +"</button>");
       var newListE = $('#zone-E');
       newListE.fadeIn();
       var colorEnd = setInterval(function () {
         newListE.css('color', 'black');
         clearInterval(colorEnd);
-      }, 35);
+      }, 0);
       buttonZoneE = buttonZoneE + 1;
-    }, 30);
+    }, 0);
 
   });
 
 
 
 
-  $('#part1').click(function () {
-  });
-  $('#part2').click(function () { });
 
-  // zone-B
-
-
-  // zone-C
-
-
-  // zone-D
-
-
-  // zone-E
 
 
 
 });
+
 
 
